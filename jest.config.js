0e1c59f28e01f094f8edbd8ca2ac55fb25047161
VINICIUS MOREIRA DA SILVA <vsilva@bbseg.com.br>
2020-08-13 19:23:33 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+    clearMocks: true,
+    collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+    coverageDirectory: "coverage",
+    // Indicates which provider should be used to instrument code for coverage
+    coverageProvider: "v8",
+    testEnvironment: "node",
+    transform: {
+        '.+\\.ts$': 'ts-jest'
+    }
+};
