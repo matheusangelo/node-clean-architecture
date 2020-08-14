70b93cdd700dc218ee51202fb76df37e4b7f036f
VINICIUS MOREIRA DA SILVA <vsilva@bbseg.com.br>
2020-08-14 12:03:47 -0300

    feat: add signunp controller validate invalid email

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,14 @@
 export class SignUpController {
   handle (httpRequest: any): any {
-    return {
-      ...httpRequest, statusCode: 400, body: new Error('Missing params: name')
+    if (!httpRequest.body.name) {
+      return {
+        ...httpRequest, statusCode: 400, body: new Error('Missing params: name')
+      }
+    }
+    if (!httpRequest.body.email) {
+      return {
+        ...httpRequest, statusCode: 400, body: new Error('Missing params: email')
+      }
     }
   }
 }
