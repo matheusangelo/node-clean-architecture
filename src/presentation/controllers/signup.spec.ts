291006bffea25b6935c71ed59563d3430013a633
VINICIUS MOREIRA DA SILVA <vsilva@bbseg.com.br>
2020-08-14 12:05:19 -0300

    test: add rules to comparation obj to boolean

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -14,4 +14,17 @@ describe('SignUpController', () => {
     expect(validate.body).toEqual(new Error('Missing params: name'))
     expect(validate.statusCode).toBe(400)
   })
+  test('Should return 400 if not send a valid request email', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'teste',
+        password: 'teste',
+        passwordConfirmation: 'teste'
+      }
+    }
+    const validate = sut.handle(httpRequest)
+    expect(validate.body).toEqual(new Error('Missing params: email'))
+    expect(validate.statusCode).toBe(400)
+  })
 })
