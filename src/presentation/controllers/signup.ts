95af6b7f9c09dede7cda095aae5e8280cb4d6cec
VINICIUS MOREIRA DA SILVA <vsilva@bbseg.com.br>
2020-08-14 14:44:45 -0300

    refactor: add class error for missing parans controller

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,15 +1,16 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
+import { MissingParamError } from '../errors/missing-param-errors'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest<any>): HttpResponse<any> {
     if (!httpRequest.body.name) {
       return {
-        ...httpRequest, statusCode: 400, body: new Error('Missing params: name')
+        ...httpRequest, statusCode: 400, body: new MissingParamError('name')
       }
     }
     if (!httpRequest.body.email) {
       return {
-        ...httpRequest, statusCode: 400, body: new Error('Missing params: email')
+        ...httpRequest, statusCode: 400, body: new MissingParamError('email')
       }
     }
   }
