628e5cdc7f5f1f6d4bbfaca7fd0f43e817c88f30
Matheus Angelo <vsilva@bbseg.com.br>
2020-08-16 12:38:21 -0300

    feat: add DI for emailValidator SignUpController

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,8 +2,16 @@ import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-errors'
 import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
+import { EmailValidator } from '../protocols/email-validator'
+import { InvalidParamError } from '../errors/invalid-param-errros'
 
 export class SignUpController implements Controller {
+  private readonly _emailValidation: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this._emailValidation = emailValidator
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = [
       'name',
@@ -15,6 +23,12 @@ export class SignUpController implements Controller {
       if (!httpRequest.body[required]) {
         return badRequest(new MissingParamError(required))
       }
+    }
+
+    const isValidEmail = this._emailValidation.isValid(httpRequest.body.email)
+
+    if (!isValidEmail) {
+      return badRequest(new InvalidParamError('email'))
     }
   }
 }
