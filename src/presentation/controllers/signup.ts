8dd544f0efa634559140dfe58b4c543cf5e6b5ad
Matheus Angelo <vsilva@bbseg.com.br>
2020-08-17 17:52:22 -0300

    feat: add validations for password and passwordConfirmation

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -17,13 +17,20 @@ export class SignUpController implements Controller {
         'password',
         'passwordConfirmation'
       ]
+
       for (const required of requiredFields) {
         if (!httpRequest.body[required]) {
           return badRequest(new MissingParamError(required))
         }
       }
 
-      const isValidEmail = this._emailValidation.isValid(httpRequest.body.email)
+      const { email, password, passwordConfirmation } = httpRequest.body
+
+      if (password !== passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
+
+      const isValidEmail = this._emailValidation.isValid(email)
 
       if (!isValidEmail) {
         return badRequest(new InvalidParamError('email'))
